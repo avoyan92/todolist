db4df000e4c99cddb28d1cc58d4c4d573489b3bd
Gevorg Avoyan <gevorg92avoyan@gmail.com>
2021-07-08 11:41:54 +0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Container from "@material-ui/core/Container";
+import Navabar from "./Heder";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+          <ExpandMoreIcon />
+        </li>
+        <Navabar />
+      </ul>
+    </Container>
   );
 }
 
---
+++ src/Heder.js
@@ -0,0 +1,18 @@
+import Diamond from "./linkedtech_img/10.jpg";
+
+
+function Navabar(){
+    return(
+        <header>
+            <nav>
+            <div className="logo">
+                
+              <img src={Diamond} />
+            </div>
+               
+
+            </nav>
+        </header>
+    )
+}
+export default Navabar;
